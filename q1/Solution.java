fbb5c9eb93f75decfcf4b3fd0bdfe6d5896f3036
Gaoming Lin <gaominglins@gmail.com>
2023-03-02 15:50:31 -0600

    ddb

--- q1/Solution.java
+++ q1/Solution.java
@@ -19,7 +19,7 @@ class Solution{
   // }
 
   public int[] twoSum(int[] nums, int target){
-    Map<Integer, Integer> map = new HashMap();
+    Map<Integer, Integer> map = new HashMap<Integer,Integer>();
     for (int i = 0; i<nums.length; i++){
       int remaining = target - nums[i];
       if (map.containsKey(remaining)){
