93b3e9c5999e4d69916d17bba8ac216a0a372b3b
Gaoming Lin <gaominglins@gmail.com>
2023-03-06 23:53:30 -0600

    eg

--- q350/Solution.java
+++ q350/Solution.java
@@ -3,25 +3,30 @@ import java.util.*;
 
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        List<Integer> nums2list = new ArrayList<Integer>(nums2.length);
-        for (int k: nums2){
-            nums2list.add(k);
-        }
-        for (int i= 0; i<nums1.length; i++){
-            for (int j = 0; j<nums2list.size();j++){
-                if (nums1[i] == nums2list.get(j)){
-                    result.add(nums1[i]);
-                    nums2list.remove(j);
-                    break;
-                }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        int i = 0, j = 0;
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]) {
+                i++;
+            }
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else{
+                arr.add(nums1[i]);
+                i++;
+                j++;
             }
         }
-        int[] result1 = new int[result.size()];
-        for (int l=0; l<result.size(); l++){
-            result1[l] = result.get(l);
+
+        int[] output = new int[arr.size()];
+        int k = 0;
+        while(k < arr.size()){
+            output[k] = arr.get(k);
+            k++;
         }
-        return result1;
+        return output;
     }
 }
-
