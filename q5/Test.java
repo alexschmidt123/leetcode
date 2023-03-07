dc7a377bea1ab7b647e65f0730a7c2b3523fd15c
Gaoming Lin <gaominglins@gmail.com>
2023-03-07 14:05:13 -0600

    non

---
+++ q5/Test.java
@@ -0,0 +1,9 @@
+package q5;
+
+public class Test {
+    public static void main(String[] args){
+        String str = "caaaaa";
+        Solution solution = new Solution();
+        System.out.println(solution.longestPalindrome(str));
+    }
+}
