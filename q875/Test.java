481720c2cb897e70b6735ca83af9c0a4eee0d042
Gaoming Lin <gaominglins@gmail.com>
2023-03-08 02:22:37 -0600

    wedcf

---
+++ q875/Test.java
@@ -0,0 +1,11 @@
+package q875;
+
+class Test{
+	public static void main(String[] args){
+		int[] piles= {312884470};
+		int h = 312884469;
+		Solution solution = new Solution();
+		int result = solution.minEatingSpeed(piles,h);
+		System.out.println(result);
+	}
+}
